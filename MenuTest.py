3238d7cd5fdf2533407b459490bbc064bc21df4d
Matthew <moofus2000@gmail.com>
2018-09-20 18:46:10 -0400

    button test half-update
    menu class is coming along, currently optimizing mouse input and function handeling. Got bored, so this is probably all I'll get done today. MenuTest.py doesn't compile, but the rest of the code hasen't been touched

--- MenuTest.py
+++ MenuTest.py
@@ -1,27 +1,43 @@
 import pygame,sys
 pygame.init()
 
-class Button:
-    def __init__(self,pos,dim,colors,label,a):
-        self.x,self.y = pos
-        self.w,self.h = dim
-        self.default_color,self.hover_color,self.press_color = colors
-        self.name = label
-        self.action = a
-
-    def pos(self):
-        return (self.x,self.y)
+class button:
+    def __init__(self):
+        self.rect = Rect(100,100,50,50)
+        self.action = test_function
+        self.hover = False
+    def mouse_input(self,fi):
+        for e in fi[0]:
+            if self.rect.collidepoint(e.pos):
+                self.hover = True
+            else
+                self.hover = False
+        for e in fi[1]:
+            if e.button
+    
 
 def test_function():
     print("You pressed the button")
 
 window = pygame.display.set_mode((512,512))
-b1 = Button((100,100),(200,50),((0,0,255),(0,50,255),(155,155,155)),"Button",test_function)
 font = pygame.font.SysFont('Courier', 20)
+
+b1 = pygame.Rect(100,100,200,200)
 
 while True:
     if pygame.event.peek(pygame.QUIT):
         pygame.quit(),sys.exit()
+
+    filtered_input = [[],[],[]]
+    for e in pygame.event.get():
+        if e.type == pygame.MOUSEMOTION:
+            filtered_input[0].append(e)
+        elif e.type == pygame.MOUSEBUTTONDOWN:
+            filtered_input[1].append(e)
+        elif e.type == pygame.MOUSEBUTTONUP:
+            filtered_input[2].append(e)
+
+    button.mouse_input(filtered_input)
 
     window.fill((255,255,255))
     fps = font.render(b1.name,True,(0,255,0))
