0ca2c86b12a7eec96270cf622b94bd429a27b03a
Matthew <moofus2000@gmail.com>
2018-09-25 00:10:20 -0400

    Finished button class
    It's really good. Don't mind if I pat myself on the back for this one. Really proud of the end result.
    In all seriousness, I finished the button class, and left it in menutest.py. In future I might want other UI elements that I will just stick in the same file. Now that I know how to utalize inputManager better, It should be easy to add stuff.
    Also deleted buttontest.py files

--- MenuTest.py
+++ MenuTest.py
@@ -3,30 +3,33 @@ from Input import InputManager as im
 pygame.init()
 
 class button:
-    def __init__(self,r,a,c = ((0,0,255),(50,50,255),(155,155,255))):
-        self.rect = r
-        self.action = a
-        self.colors = c
+    def __init__(self,_rect,_action,_text,font,_colors = ((0,0,255),(50,50,255),(155,155,255))):
+        self.rect = _rect
+        self.action = _action
+        self.colors = _colors
         self.hover = False
-        self.held = False
+        
+        self.text = _text
+        self.label = font.render(_text,True,(0,255,0))
+        self.text_pos = (self.rect.x+(self.rect.width-self.label.get_width())//2,\
+                         self.rect.y+(self.rect.height-self.label.get_height())//2)
+    
     def update(self):
         for e in im.mousemotion():
             self.hover = self.rect.collidepoint(e.pos)
         for e in im.mbup():
             if self.hover and e.button == 1:
-                self.action()
-    def bg(self):
-        if self.hover and pygame.mouse.get_pressed()[0]:
-            return self.colors[2]
-        elif self.hover:
-            return self.colors[1]
-        else:
-            return self.colors[0]
+                self.action(self.text)
+    
+    def draw(self,window=None):
+        c = self.hover*(1+pygame.mouse.get_pressed()[0])
+        pygame.draw.rect(window,self.colors[c],self.rect)
+        window.blit(self.label,self.text_pos)
     
 class __main__:
     
-    def test_function():
-        print("test")
+    def test_function(x="yay"):
+        print(x)
     
     def __init__(self):
         im.__init__()
@@ -34,18 +37,17 @@ class __main__:
         window = pygame.display.set_mode((512,512))
         font = pygame.font.SysFont('Courier', 20)
 
-        b1 = button(pygame.Rect(100,100,50,50),__main__.test_function)
+        b1 = button(pygame.Rect(100,100,120,50),__main__.test_function,"button 1",font)
+        b2 = button(pygame.Rect(100,160,120,50),__main__.test_function,"button 2",font)
 
         while True:
             im.manage_input()
-
             b1.update()
+            b2.update()
+            
             window.fill((255,255,255))
-            
-            pygame.draw.rect(window,b1.bg(),b1.rect)
-            
-            fps = font.render("test",True,(0,255,0))
-            window.blit(fps,(b1.rect.x,b1.rect.y))
+            b1.draw(window)
+            b2.draw(window)
 
             pygame.display.flip()
 __main__()
