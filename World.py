eb2325b631a55401a85490d137a7af0a741ec4ed
Matthew <moofus2000@gmail.com>
2018-09-26 08:50:53 -0400

    Matthew

--- World.py
+++ World.py
@@ -1,35 +1,31 @@
 import pygame, sys, math
 from Terrain import TerrainGenerator
-from Camera import Player
 from Draw import Render
 from Input import InputManager as im
   
 class World:
-    def __init__(self,_window):
-        self.window = _window
-        self.w,self.h = pygame.display.get_surface().get_size()
-        
+    def __init__(self,width,height):
+        self.w = width
+        self.h = height
+        self.window = pygame.Surface((width,height))
+                
         #terrain generation maybe
-        self.world = TerrainGenerator()
-        self.world.pillars()
-        self.world.hideFaces()
-
-        #controlled camera, will probably move code somewhere else
-        self.p1 = Player((5,5,5))
+        self.terrain = TerrainGenerator()
+        self.terrain.pillars()
+        self.terrain.hideFaces()
     
-    def update(self,dt):
+    def update(self,player,dt):
         #keep mouse in center of screen
         pygame.mouse.set_pos(256,256)
         pygame.event.clear(pygame.MOUSEMOTION)
 
-        #update player (again, temporary)
-        self.p1.update(self.world,dt)
-
         #update cubes in terrain
-        for cube in self.world.cube_list:
-            cube.relative_faces(self.p1.pos())
+        for cube in self.terrain.cube_list:
+            cube.relative_faces(player.pos())
         
         #graphics
         self.window.fill((255,255,255))
-        Render.render(self.window,self.world.cube_list,self.p1)
+        Render.render(self.window,self.terrain.cube_list,player)
         pygame.draw.circle(self.window,(0,0,0),(self.w//2,self.h//2),3)
+
+        return self.window
--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import pygame, sys, math
 from World import World
+from Camera import Player
 from Input import InputManager as im
 
 class __main__:
@@ -12,9 +13,10 @@ class __main__:
 
         #variables +other cool stuff
         im.__init__()
-        paused = True
+        paused = False
         font = pygame.font.SysFont('Courier', 20)
-        dim1 = World(window)
+        dim1 = World(w,h)
+        p1 = Player((-5,5,-5))
         
         #game loop
         while True:
@@ -30,11 +32,14 @@ class __main__:
                     #pygame.mouse.set_visible(paused)
                     print("Paused:",paused)
                     _keydown.remove(e)
-            
+
+            window.fill((255,255,255))
             if not paused:
-                dim1.update(dt)
+                p1.update(dim1.terrain,dt)
+                world_surface = dim1.update(p1,dt)
             else:
                 pass
+            window.blit(world_surface,(0,0))
 
             #print out fps
             fps = font.render(str(int(clock.get_fps())),True,(0,255,0))
