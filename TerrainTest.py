b90f78e56847daff01962bb4f277be41153a47e3
Matthew <moofus2000@gmail.com>
2018-10-17 01:51:50 -0400

    idk
    these commit descriptions/names are gonna be real noncommittal until some actual progress is made

--- TerrainTest.py
+++ TerrainTest.py
@@ -4,32 +4,53 @@ import math
 pygame.init()
 im.__init__()
 
-class Square:
-    def __init__(self,_rect,_color):
-        self.rect = rect
-        self.color = color
+class Quadtree:
+    def __init__(self):
+        self.quadrants = [None]*4
 
-    def draw(self,window):
-        pygame.draw.rect(window,self.color,rect,5)
+    def draw(self):
+        pass
+
+class Square:
+    def __init__(self,pos):
+        self.x,self.y = pos
+        
+    def draw(self,window,offset):
+        x,y = (self.x*50-offset[0],(-1-self.y)*50-offset[1])
+        pygame.draw.polygon(window,(0,0,255),((x,y),(x+50,y),(x+50,y+50),(x,y+50)),3)
 
 class Camera:
     def __init__(self):
-        self.x = 0; self.y = 0
+        self.x = -100; self.y = 400
         self.keys = [pygame.K_w,pygame.K_s,pygame.K_a,pygame.K_d]
+        self.pressed = [False]*4
+    
     def update(self):
-        pressed = [False]*4
-        
         for e in im.keydown():
             for i in range(0,4):
-                pressed[i] = pressed[i] or e.key == self.keys[i]
+                if e.key == self.keys[i]: self.pressed[i] = True
         for e in im.keyup():
             for i in range(0,4):
-                pressed[i] = not(pressed[i] or e.key == self.keys[i])
-        if pressed[0]: self.y += .1
-        if pressed[1]: self.y -= .1
-        if pressed[2]: self.x -= .1
-        if pressed[3]: self.x += .1
-        
+                if e.key == self.keys[i]: self.pressed[i] = False
+        if self.pressed[0]: self.y += 5
+        if self.pressed[1]: self.y -= 5
+        if self.pressed[2]: self.x -= 5
+        if self.pressed[3]: self.x += 5
+
+class Cursor:
+    def __init__(self):
+        self.x = 0; self.y = 0
+    
+    def update(self):
+        for e in im.keydown():
+            if e.key == pygame.K_UP: self.y += 1
+            elif e.key == pygame.K_DOWN: self.y -= 1
+            elif e.key == pygame.K_LEFT: self.x -= 1
+            elif e.key == pygame.K_RIGHT: self.x += 1
+
+    def draw(self,window,offset):
+        x,y = (self.x*50-offset[0],(-1-self.y)*50-offset[1])
+        pygame.draw.polygon(window,(0,0,0),((x,y),(x+50,y),(x+50,y+50),(x,y+50)),3)
         
 class __main__:
     def __init__(self):
@@ -37,13 +58,35 @@ class __main__:
         font = pygame.font.SysFont('Courier', 20)
         clock = pygame.time.Clock()
         cam = Camera()
+        board = Square((0,0))
+        square_list = [board]
+        cursor = Cursor()
         
-        while True
+        while True:
+            im.manage_input()
             cam.update()
+            cursor.update()
+
+            for e in im.keydown():
+                if e.key == pygame.K_RETURN:
+                    s = Square((cursor.x,cursor.y))
+                    
+
+                    
+                    square_list.append(s)
+            
+            x,y = (cam.x,-cam.y)
             
             window.fill((255,255,255))
-            pygame.draw.line(window,(155,155,155),())
+            for s in square_list: s.draw(window,(x,y))
+            cursor.draw(window,(x,y))
+            pygame.draw.line(window,(255,0,0),(-x,0),(-x,500))
+            pygame.draw.line(window,(255,0,0),(0,-y),(500,-y))
+
+            fps = font.render(('%.1f'%cursor.x)+", "+('%.1f'%cursor.y),True,(0,255,0))
+            window.blit(fps,(20,20))
             
             pygame.display.flip()
             clock.tick(30)
+
 __main__()
